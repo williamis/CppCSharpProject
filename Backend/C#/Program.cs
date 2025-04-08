bd92f64416c3071b3f46798bde24bc0596c903e0
Ville M <vilqst.og@gmail.com>
2025-04-08 19:08:40 +0300

    Ensimmäinen versio – toimiva C++ DLL + C# sovellus

---
+++ Backend/C#/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("CppLibrary.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int add(int a, int b);
+
+    static void Main()
+    {
+        int tulos = add(3, 4);
+        Console.WriteLine($"3 + 4 = {tulos}");
+    }
+}
